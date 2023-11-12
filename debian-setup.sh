bcc15a4ce2ad608ed6a034aa7740f7e02c028e29
wormsparty <wormsparty@gmail.com>
2023-11-12 10:35:35 +0100

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -5,15 +5,6 @@ if [ "`id -u`" -eq 0 ]; then
 	exit 1
 fi
 
-# 1. Install signal repository
-if [ ! -f /usr/share/keyrings/signal-desktop-keyring.gpg ]; then
-	sudo bash -c "wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /usr/share/keyrings/signal-desktop-keyring.gpg"
-	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
-	sudo apt-get update
-else
-	echo "Ignoring signal keyring, looks already done."
-fi
-
 # VSCodium repository
 if [ ! -f /usr/share/keyrings/vscodium-archive-keyring.gpg ]; then
 	wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
@@ -29,7 +20,7 @@ else
 fi
 
 # 2. Install packages
-sudo apt-get install curl rclone gimp inkscape vlc transmission-gtk blender goldendict-webengine foliate audacious signal-desktop libreoffice-writer firefox-esr qt5ct qt5-style-kvantum neovim codium flatpak gnome-software-plugin-flatpak
+sudo apt-get install curl rclone gimp inkscape vlc transmission-gtk blender goldendict-webengine audacious libreoffice-writer firefox-esr qt5ct qt5-style-kvantum neovim codium flatpak gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 if [ ! -f /usr/local/bin/up ]; then
