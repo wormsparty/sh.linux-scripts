436fd4464322d1089dd2aee7796a17f800eab06e
wormsparty <wormsparty@gmail.com>
2023-11-26 13:14:39 +0100

    Add conditions

--- raspberry.sh
+++ raspberry.sh
@@ -2,33 +2,39 @@
 
 sudo apt-get install vim kodi kodi-peripheral-joystick openssh-server unison lightdm 
 
-cat << EOT | sudo tee /etc/locale.gen
+if ! grep UTF-8 /etc/locale.gen; then
+	cat << EOT | sudo tee /etc/locale.gen
 en_GB.UTF-8 UTF-8
 fr_CH.UTF-8 UTF-8
 EOT
+	
+	sudo locale-gen
+	sudo localectl set-locale LANG=en_GB.UTF-8
+fi
 
-sudo locale-gen
-sudo localectl set-locale LANG=en_GB.UTF-8
-
-cat << EOT | sudo tee /etc/modprobe.d/raspi-blacklist.conf 
+if ! grep brcmfmac /etc/modprobe.d/raspi-blacklist.conf; then
+	cat << EOT | sudo tee /etc/modprobe.d/raspi-blacklist.conf 
 blacklist brcmfmac
 blacklist brcmutil
 blacklist bluetooth
 EOT
 
-sudo modprobe -r brcmfmac
-sudo modprobe -r brcmutil
-sudo depmod -ae
-sudo update-initramfs -u
+	sudo modprobe -r brcmfmac
+	sudo modprobe -r brcmutil
+	sudo depmod -ae
+	sudo update-initramfs -u
+fi
 
 sudo systemctl disable bluetooth
 sudo systemctl enable ssh
 
-cat << EOT | sudo tee /etc/lightdm/lightdm.conf
+if ! grep kodi /etc/lightdm/lightdm.conf; then
+	cat << EOT | sudo tee /etc/lightdm/lightdm.conf
 [SeatDefaults]
 autologin-user=$USER
 user-session=kodi
 EOT
+fi
 
 # Replace default green to orange prompt 
 sed -i 's/\[\\033\[01;32m\\\]/\[\\033\[01;33m\\\]/g' ~/.bashrc
