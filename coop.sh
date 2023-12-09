440211caec521974fb8855c1ed0d21fdbf50450b
wormsparty <wormsparty@gmail.com>
2023-12-09 23:49:04 +0100

    Update coop.sh

--- coop.sh
+++ coop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cd "`dirname $0`"
-
 #
-# List joysticks, we expect to have exactly 2.
-# It doesn't matter which one we attach to which window, since the executable is stictly
-# the same for both, it will have the same configuration / state
+# This scripts allows to run the same executable twice, and blacklist the joystick assigned to the other window.
+# This supposes the executable can be run in windowed mode. If not, you may want to wrap it in a "weston" window.
+#
+# First, list the joysticks, we expect to have exactly 2.
+# It doesn't matter which one we attach to which window, since the executable is strictly
+# the same for both, it will have the same configuration / state.
 #
 CONTROLLER_LIST=$(ls -l /dev/input/by-id/ | grep joystick |  awk '{gsub("-joystick", ""); gsub("-event", ""); print $9}' | uniq)
 CONTROLLER_COUNT=$(echo "$CONTROLLER_LIST" | wc -l)
@@ -20,9 +21,7 @@ CONTROLLER_2=$(echo $CONTROLLER_LIST | sed -n '2 p')
 BLACKLIST_1=$(echo $(ls -l /dev/input/by-id/ | grep joystick | grep -wv $CONTROLLER_1 | awk '{print "--blacklist=/dev/input/by-id/" $9;}' ) )
 BLACKLIST_2=$(echo $(ls -l /dev/input/by-id/ | grep joystick | grep -wv $CONTROLLER_2 | awk '{print "--blacklist=/dev/input/by-id/" $9;}' ) ) 
 
-EXEC="/home/mob/Documents/sm64ex-coop/build/us_pc/run.sh"
-
-# Run the game twice. 
+# Run the executable twice. 
 # Note: Use win+left and win+right to move the windows to use the left and right half, they may both start at the same position
-firejail --noprofile $BLACKLIST_1 "$EXEC" &
-firejail --noprofile $BLACKLIST_2 "$EXEC" &
+firejail --noprofile $BLACKLIST_1 $@ &
+firejail --noprofile $BLACKLIST_2 $@ &
